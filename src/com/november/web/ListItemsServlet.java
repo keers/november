02febe9b95ce00cf00abcefe7d40329d5ce64676
Sergei Kirsanov <sergei.kirsanov@gmail.com>
2012-03-03 12:20:56 +0400

    add

--- src/com/november/web/ListItemsServlet.java
+++ src/com/november/web/ListItemsServlet.java
@@ -1,11 +1,17 @@
 package com.november.web;
 
+import com.november.model.Item;
+import com.november.model.ItemDAO;
+import com.november.model.ItemDAOImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
 /**
  * User: sergeikirsanov
@@ -13,42 +19,27 @@ import java.io.PrintWriter;
  * Time: 12:29 PM
  */
 
-@WebServlet(urlPatterns = {"/hello"})
+@WebServlet(urlPatterns = {"/items"})
 public class ListItemsServlet extends HttpServlet {
 
-
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        ItemDAO itemDAO = ItemDAOImpl.getInstance();
 
-        PrintWriter out = null;
+        List<Item> items = itemDAO.getItems();
+
+
+        request.setAttribute("items", items);
+        RequestDispatcher rd = request.getRequestDispatcher("/jsp/items.jsp");
+
         try {
-            out = response.getWriter();
-
-            out.print("Hello, NCEDU!");
+            rd.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-        /*
-        DataBean b = new DataBean();
-
-        b.setSomeOtherValue("OTHER VALUE");
-        b.setSomeValue("SOME VALUE");
-        DataBean b2 = new DataBean();
-
-        b2.setSomeOtherValue("OTHER VALUE __ 2");
-        b2.setSomeValue("SOME VALUE __ 2");
-
-        List<DataBean> l = new ArrayList<DataBean>();
-        l.add(b);
-        l.add(b2);
-
-        request.setAttribute("data", b);
-        request.setAttribute("dataList", l);
-        RequestDispatcher rd = request.getRequestDispatcher("jsp/some.jsp");
-        rd.forward(request, response);
-        */
     }
-
 
 }
--- webapp/index.jsp
+++
@@ -1,3 +0,0 @@
-<%
-    response.sendRedirect("/november/hello");
-%>
